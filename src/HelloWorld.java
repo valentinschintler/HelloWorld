1d1acc5bbba08c36c90cac95a2a097939405ab57
valentinschintler <s51125@edu.campus02.at>
2022-09-16 19:25:32 +0200

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hello World"); // Strg + D --> Zeile kopieren
+        System.out.println("Zeile 6");
+        System.out.println("Zeile 7");
+    }
+
+
+}
